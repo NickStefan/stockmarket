d33ab82e687a7becacf357fb84e061f0d5e08f2a
Nick Stefan <NickStefan12@gmail.com>
2016-03-20 21:09:36 -0700

    notes on blocking vs not blocking

--- account_service/account.go
+++ account_service/account.go
@@ -3,16 +3,22 @@ package main
 import (
 	"net/http"
 	"fmt"
- 	// "time"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		// time.Sleep(5000 * time.Millisecond) 
-		// Ive discovered new problems...
+
 		// if this request blocks in response,
 		// the market_service cant keep handling trades
-		// this is where node.js shines (non blocking)
+
+		// that should actually be okay. we probably dont want to do any trading
+		// if the order fufillment is down!
+
+		// but, we dont want to be limited by each individual trade being an HTTP
+		// round trip. what if we used batching?
+
+		// what if we batch all of the trades in the market_service every 100ms
+		// we then every 100ms send the trades to the account_service?
 
 		// decoder := json.NewDecoder(req.Body)
 		fmt.Println("hello account")
