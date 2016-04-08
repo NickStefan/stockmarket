b450df2986f9ca16bf0e4d56c2652960a0a983bb
Nick Stefan <NickStefan12@gmail.com>
2016-04-07 23:38:51 -0700

    updated todo list

--- readme.md
+++ readme.md
@@ -4,17 +4,35 @@ _now in golang!_
 
 ### TODO
 - [x] ledger service  
+  - [x] listen for http (from orderbook)
+  - [x] track quantity and asset for each user (cash is an asset)  
+
 - [x] orderbook service  
+  - [ ] listen for http (for submitting of orders)  
+  - [x] priotiy queues for buy and sell orders
+  - [x] dequeue priority queues into trades
   - [x] message to ledger service  
   - [x] message to ticker service  
-  - [ ] listen for http (for submitting of orders)
 
-- [ ] kafka (message queue between services)  
 
 - [ ] ticker service (chart and quote stream)  
-  - temporarily persist past 24 hours, and socket stream rest
+  - [ ] on price data, publish quote to "QUOTE" channel; rate limit 1 / second  
+  - [ ] on price data, add to cache of last 60 seconds of trades  
+  - [ ] every 60 seconds, calc minute data, persist to DB, publish to "CHART" channel  
 
 - [ ] web service (front end data)  
+  - [ ] serve front end javascript  
+  - [ ] handle accounts and oath  
+
 - [ ] web client (front end ui)  
+  - [ ] enable listening to "QUOTE" and "CHART" channels  
+  - [ ] graph "CHART" data into a stock chart  
+  - [ ] display account info  
+  - [ ] place orders  
+
 - [ ] bot service (automate trades)  
+  - [ ] write trading bots   
+  - [ ] make a front end UI for creating bots
+
+- [ ] kafka (message queue between services)  
 
--- ticker_service/ticker.go
+++ ticker_service/ticker.go
@@ -20,37 +20,7 @@ func main() {
 
     hash := make(map[string][]Trade)
 
-    // HOW SHOULD WE STORE THE DATA?
-    // if speed not an issue, we would always add to mongo, and then aggregate the time series from mongo 
-    // if speed is issue, we should store the series in redis, and then transform to requested series
-    // we want speed, but dont want to store ALL of history in redis
-
-    // need to impliment an aggregation framework on top of our redis store?
-    // any data requested older than today, we then go to mongo?
-
-    // older than n (e.g. today), use mongo aggregation query
-    // today, get the pre-formatted ticks from redis
-
-    // when we insert trades to redis ticker, preformat it for multiple ticker styles on multiple keys
-    // then, end of day, do a job to put ticker data into actual DB
-
-    // http://blog.mongodb.org/post/65517193370/schema-design-for-time-series-data-in-mongodb
-    
-    // {
-    //       timestamp_hour: ISODate("2013-10-10T23:00:00.000Z"),
-    //       type: “memory_used”,
-    //       values: {
-    //         0: { 0: {vol: 1000, price: 10.05}, …, 59: {vol: 1000, price: 10.05} },
-    //         1: { 0: {vol: 1000, price: 10.05}, …, 59: {vol: 1000, price: 10.05} },
-    //         …,
-    //         58: { 0: {vol: 1000, price: 10.05}, …, 59: {vol: 1000, price: 10.05} },
-    //         59: { 0: {vol: 1000, price: 10.05}, …, 59: {vol: 1000, price: 10.05} }
-    //     }
-    // }
-
-    // {$set: {“values.59.59”: {vol: 1000, price: 10.05} } }
-
-    // OR only store minutes (we dont need 1 sec or 10 sec charts)
+    // only store minutes (we dont need 1 sec or 10 sec charts)
 
     // {
     //       timestamp_hour: ISODate("2013-10-10T23:00:00.000Z"),
@@ -67,30 +37,33 @@ func main() {
     // {$set: {“values.59”: {vol: 1000, open: 10.65, close: 11.55, high: 12.00, low: 10.00 } }
 
     // basic charting:
-    // map minute based data to correct format when querying server DB
+    // aggregate minute based data to correct format time ranges
+
+    // what will an aggregation query look like? "I want the last <X>(5?) <range>(days?) <duration>(hour?) chart"
+    // $match stock <X> * <range>
+    // $unwind values
+    // $groupby <duration> (want array of duration groups)
+    // $sum vol, $min low, $max hi, open, close of first?
+    // $map appropriate time stamps?
     
     // real time chart:
-    // then publish each new complete minute doc
-    // front end will merge that into the data format of the queried for chart
+    // front end will merge published minute info into the chart's range
 
     // 1 second ticker data will be separate channel, not persisted in a collection
 
     // on price data
-    // throttle to every 1 second
-    //      publish to ticker channel
-    //      call chartData function, which is in turn throttled
-    //          to update minute doc only when its been a minute(?)
+    //      - publish to ticker channel, rate limit to one / second
+    //      - add to cache of last 60 seconds of trades 
 
-    // question what to do when no trades are happening? update docs? 
-    // if we could have "defaults" when aggregating docs OR saving docs, could help?
-
-    // do we need an interval method to run every 60 seconds: http://stackoverflow.com/questions/16466320/is-there-a-way-to-do-repetitive-tasks-at-intervals-in-golang
-    
     // every 60 seconds,
-    //      for each stock symbols
-    //          calc high, open, low, close, vol for last 60 seconds (cache last state to solve missing values?)
-    //          clear cache?
+    //      for each stock
+    //          - calculate high, open, low, close, vol for last 60 seconds (cache last state to solve missing values?)
+    //          - update DB
+    //          - publish latest minute info
+    //          - clear cache
 
+    // http://stackoverflow.com/questions/16466320/is-there-a-way-to-do-repetitive-tasks-at-intervals-in-golang
+    
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
         var payload [2]Trade
         decoder := json.NewDecoder(r.Body)
