0c3248c9f0e43d560874588fc8a67b2d94d33bc9
Nick Stefan <NickStefan12@gmail.com>
2016-04-07 22:58:55 -0700

    notes on how to handle chart ticker

--- ticker_service/ticker.go
+++ ticker_service/ticker.go
@@ -50,6 +50,47 @@ func main() {
 
     // {$set: {“values.59.59”: {vol: 1000, price: 10.05} } }
 
+    // OR only store minutes (we dont need 1 sec or 10 sec charts)
+
+    // {
+    //       timestamp_hour: ISODate("2013-10-10T23:00:00.000Z"),
+    //       type: “memory_used”,
+    //       values: {
+    //         0: {vol: 1000, open: 10.05, close: 10.55, high: 11.00, low: 10.00 },
+    //         1: {vol: 1000, open: 10.55, close: 10.60, high: 11.00, low: 10.50 },
+    //         …,
+    //         58: {vol: 1000, open: 10.65, close: 10.80, high: 11.00, low: 10.60 },
+    //         59: {vol: 1000, open: 10.65, close: 11.55, high: 12.00, low: 10.00 } 
+    //       }
+    //     }
+    // }
+    // {$set: {“values.59”: {vol: 1000, open: 10.65, close: 11.55, high: 12.00, low: 10.00 } }
+
+    // basic charting:
+    // map minute based data to correct format when querying server DB
+    
+    // real time chart:
+    // then publish each new complete minute doc
+    // front end will merge that into the data format of the queried for chart
+
+    // 1 second ticker data will be separate channel, not persisted in a collection
+
+    // on price data
+    // throttle to every 1 second
+    //      publish to ticker channel
+    //      call chartData function, which is in turn throttled
+    //          to update minute doc only when its been a minute(?)
+
+    // question what to do when no trades are happening? update docs? 
+    // if we could have "defaults" when aggregating docs OR saving docs, could help?
+
+    // do we need an interval method to run every 60 seconds: http://stackoverflow.com/questions/16466320/is-there-a-way-to-do-repetitive-tasks-at-intervals-in-golang
+    
+    // every 60 seconds,
+    //      for each stock symbols
+    //          calc high, open, low, close, vol for last 60 seconds (cache last state to solve missing values?)
+    //          clear cache?
+
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
         var payload [2]Trade
         decoder := json.NewDecoder(r.Body)
